8f80d6910f883992675c32191638f667aa4b56f7
Muhammad Ammar Tanweer <49334070+MUHAMMAD-AMMAR-TANWEER@users.noreply.github.com>
2022-03-04 21:43:06 +0500

    labelling of the folders and coverting them in numpy is done

--- faces_train.py
+++ faces_train.py
@@ -1,10 +1,15 @@
 import os
 from PIL import Image
 import numpy as np
+import cv2
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 img_dir = os.path.join(BASE_DIR, "images")
 
+face_cascade = cv2.CascadeClassifier("data/haarcascade_frontalface_alt2.xml")
+
+current_id = 0
+label_ids = {}
 y_labels = []
 x_train = []
 
@@ -15,10 +20,25 @@ for root, dirs, files in os.walk(img_dir):
             path = os.path.join(root, file)
             label = os.path.basename(root).replace(" ", "-").lower()
             # print(path, label)
+            if not label in label_ids:
+                label_ids[label] = current_id
+                current_id += 1
+
+            id_ = label_ids[label]
             pil_image = Image.open(path).convert(
                 "L"
             )  # this converts Image into grayscale
             image_array = np.array(
                 pil_image, "uint8"
             )  # use uint8 because 8 bit makes 255 0-255 positive numbers
-            print(image_array)
+            print(id_)
+
+            faces = face_cascade.detectMultiScale(
+                image_array, scaleFactor=1.5, minNeighbors=5
+            )
+
+            for (x, y, w, h) in faces:
+                roi = image_array[y : y + h, x : x + w]
+                x_train.append(roi)
+                y_labels.append(id_)
+print(label_ids)

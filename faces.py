9c912ab91722d95e7309f516096a034d5aa72ce2
Muhammad Ammar Tanweer <49334070+MUHAMMAD-AMMAR-TANWEER@users.noreply.github.com>
2022-03-04 16:42:55 +0500

    face recognition is done with haarcascade

--- faces.py
+++ faces.py
@@ -2,9 +2,17 @@ import cv2
 import numpy as np
 
 
+face_cascade = cv2.CascadeClassifier("data/haarcascade_frontalface_alt2.xml")
+
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        print(x, y, w, h)
+        roi_gray = gray[y : y + h, x : x + w]
+        roi_color = frame[y : y + h, x : x + w]
 
     cv2.imshow("frame", frame)
     if cv2.waitKey(20) & 0xFF == ord("q"):

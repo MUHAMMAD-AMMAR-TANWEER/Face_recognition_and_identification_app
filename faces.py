ed61b78b2f304e027b2bab78ba8cad6d7ee59b97
Muhammad Ammar Tanweer <49334070+MUHAMMAD-AMMAR-TANWEER@users.noreply.github.com>
2022-03-05 08:36:16 +0500

    made the faical identification system

--- faces.py
+++ faces.py
@@ -1,8 +1,16 @@
 import cv2
 import numpy as np
+import pickle
 
 
 face_cascade = cv2.CascadeClassifier("data/haarcascade_frontalface_alt2.xml")
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainner.yml")
+
+
+with open("labels.pickle", "rb") as f:
+    og_labels = pickle.load(f)
+    labels = {v: k for k, v in og_labels.items()}
 
 cap = cv2.VideoCapture(0)
 while True:
@@ -13,6 +21,20 @@ while True:
         print(x, y, w, h)
         roi_gray = gray[y : y + h, x : x + w]
         roi_color = frame[y : y + h, x : x + w]
+
+        # recognizing faces
+
+        id_, conf = recognizer.predict(roi_gray)
+        if conf > 45:
+            print(labels[id_])
+            font = cv2.FONT_HERSHEY_COMPLEX
+            names = labels[id_]
+            color = (255, 255, 255)
+            line_thickness = 2
+
+            cv2.putText(
+                frame, names, (x, y), font, 1, color, line_thickness, cv2.LINE_AA
+            )
 
         color = (0, 255, 0)
         color_line_width = 2
